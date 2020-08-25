fd32d0fa70ba77a476888c054ac75c38c52f8fd3
dgoncharenko <dgoncharenko@crispwp.com>
2020-08-26 00:56:43 +0300

    style spinner

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -1,8 +1,14 @@
 import React from 'react';
+
+import spinnerSvg from './assets/spinner.svg'
 import './spinner.css';
 
 const Spinner = () => {
-	return <div>Loading...</div>
+	return (
+		<div className="spinner">
+			<img src={spinnerSvg} alt=""/>
+		</div>
+	)
 };
 
 export default Spinner;
